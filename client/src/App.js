f577b67ca963f13ab0a7ccd3f3c8db9343e89bb5
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-11-19 21:30:22 +0100

    Add folder

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import MainLayout from './components/layout/MainLayout/MainLayout';
+
+import Home from './components/pages/Home/HomePage';
+import Posts from './components/pages/Posts/PostsPage';
+import AddPost from './components/pages/NewPost/AddPost';
+import SinglePost from './components/pages/SinglePost/SinglePost';
+import Contact from './components/pages/Contact/ContactPage';
+import NotFound from './components/pages/NotFound/NotFoundPage';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <MainLayout>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/posts' exact component={Posts} />
+          <Route path='/posts/new' exact component={AddPost} />
+          <Route path='/posts/test' exact component={SinglePost} />
+          <Route path='/contact' exact component={Contact} />
+          <Route component={NotFound} />
+        </Switch>
+      </MainLayout>
+    )
+  }
+}
+
+export default App;
