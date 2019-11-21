74bdc7f65305e7ea00d5c0a30f039bce7f9e712a
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-11-21 16:49:16 +0100

    Add axios, devTools, config.js and async/await

---
+++ client/src/components/features/PostsCounter/PostsCounter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class PostsCounter extends React.Component {
+
+    render() {
+        return (
+            <div>Posts Counter</div>
+        );
+    }
+};
+
+export default PostsCounter;
