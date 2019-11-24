0c3d7d0955c545eeed6b0f2d694667081ac67c04
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-11-24 17:54:53 +0100

    Add mongoDB and nodemon - modified package.json

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const config = require('./config');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -12,8 +13,13 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use('/api', postRoutes);
 
+//connects our back end code with database
+mongoose.connect(config.DB, {useNewUrlParser: true});
+let db = mongoose.connection;
+
+db.once('open', () => console.log('Connected to the database'));
+db.on('error', (err) => console.log('Error ' + err));
+
 app.listen(config.PORT, function() {
   console.log('Server is running on Port:', config.PORT);
 });
-
-console.log('abs');
