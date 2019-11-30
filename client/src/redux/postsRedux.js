2a89b797eb586555a8f10c0281b89237e62d81aa
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-11-30 21:41:09 +0100

    Modified Redux add thunk addPostRequest

--- client/src/redux/postsRedux.js
+++ client/src/redux/postsRedux.js
@@ -61,6 +61,22 @@ export const loadSinglePostRequest = id => {
     };
   };
 
+export const addPostRequest = (post) => {
+    return async dispatch => {
+
+        dispatch(startRequest());
+        try {
+
+            let res = await axios.get(`${API_URL}/posts`, post);
+            await new Promise((resolve, reject) => setTimeout(resolve, 2000));
+            dispatch(endRequest());
+            
+        } catch(e) {
+            dispatch(errorRequest(e.message));
+        }
+    }
+}
+
 /* INITIAL STATE */
 
 const initialState = {
