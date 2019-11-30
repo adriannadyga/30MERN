5db340016a041147ab8f0a70af824d4c395a4ef9
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-11-30 21:33:46 +0100

    Add style to PostForm component

--- client/src/components/features/PostForm/PostForm.js
+++ client/src/components/features/PostForm/PostForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import './PostForm.scss';
 
 import Editor from 'react-medium-editor';
 import 'medium-editor/dist/css/medium-editor.css';
