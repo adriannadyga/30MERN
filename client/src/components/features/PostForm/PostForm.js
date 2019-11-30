16b480afc682844b6ca67878c7cf5cc6492439b9
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-11-30 21:31:28 +0100

    Add form handling

--- client/src/components/features/PostForm/PostForm.js
+++ client/src/components/features/PostForm/PostForm.js
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import Editor from 'react-medium-editor';
+import 'medium-editor/dist/css/medium-editor.css';
+import 'medium-editor/dist/css/themes/default.css';
+
 import TextField from '../../common/TextField/TextField';
 import SectionTitle from '../../common/SectionTitle/SectionTitle';
 import Button from '../../common/Button/Button';
@@ -16,24 +20,61 @@ class PostForm extends React.Component {
         }
     }
 
+    handleChange = (e) => {
+        const { post } = this.state;
+        this.setState({ post: { ...post, [e.target.name]: e.target.value }});
+    }
+
+    handleEditor = (text) => {
+        const { post } = this.state;
+        this.setState({ post: { ...post, content: text}});
+    }
+
     render() {
 
         const { post } = this.state;
+        const { handleChange, handleEditor } = this;
 
         return(
-            <div>
+            <form>
                 <TextField
                     label="Title"
                     value={post.title}
+                    onChange={handleChange}
+                    name="title"
+                />
+
+                <TextField
+                    label="Author"
+                    value={post.author}
+                    onChange={handleChange}
+                    name="author"
                 />
 
                 <SectionTitle>Edit post content</SectionTitle>
 
+                <Editor
+                className="content-editor"
+                text={post.content}
+                onChange={handleEditor}
+                options={{placeholder: false, toolbar: { buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3'] } }}
+                />
+
                 <Button variant="primary">Add post</Button>
 
-            </div>
+            </form>
         );
     }
+};
+
+PostForm.propTypes = {
+    posts: PropTypes.arrayOf(
+        PropTypes.shape({
+            title: PropTypes.string.isRequired,
+            content: PropTypes.string.isRequired,
+            author: PropTypes.string.isRequired,
+        })
+    ),
 };
 
 export default PostForm;
