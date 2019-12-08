cdf17d462e5da3ae4999e7edb83e25ded6f107eb
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-12-08 14:27:56 +0100

    Changes in config.js

--- server/config.js
+++ server/config.js
@@ -1,4 +1,7 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
-    PORT: 8000,
-    DB: 'mongodb+srv://kodilla1:kodilla1@cluster0-z4q7j.mongodb.net/test?retryWrites=true&w=majority',
-  };
+  PORT: process.env.PORT,
+  DB: process.env.DB,
+};
